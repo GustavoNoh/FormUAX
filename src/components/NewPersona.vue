90c8d793f087aa194bb6d3d620820c24828eab79
GustavoNoh <gustavo.19070048@itsmotul.edu.mx>
2023-03-04 16:31:02 -0600

    Diseño final con enlaces, falta boton agregar

--- src/components/NewPersona.vue
+++ src/components/NewPersona.vue
@@ -1,71 +1,73 @@
 <script>
-  import axios from 'axios'
-  import btnEnviar from '../components/btnSubmit.vue'
-  
-  export default {
-  
-    data() {
-      return {
-        
-        Nombre: '',
-        Edad:  '',
-        EstadoCivil: " ",
-        Trabajo:" ",
-        Residencia: " ",
-        Cita: " ",
-        CitaAutor: " ",
-        Bio: " ",
-        Personalidad1: " ",
-        Personalidad2: " ",
-        Personalidad3: " ",
-        Personalidad4: " ",
-        Objetivos: [],
-        Frustraciones: [],
-        Motivaciones: [],
-        Marcas: " "
-  
-      }
-    },
-  
-    mounted() {
-    },
-    components:{
-      btnEnviar
-    },
-  
-    methods: {
-      
-      Registro() {  
-        axios
+
+
+import axios from 'axios'
+import btnEnviar from '../components/btnSubmit.vue'
+
+export default {
+
+  data() {
+    return {
+
+      Nombre: '',
+      Edad: '',
+      EstadoCivil: " ",
+      Trabajo: "",
+      Residencia: "",
+      Cita: "",
+      CitaAutor: "",
+      Bio: "",
+      Personalidad1: "",
+      Personalidad2: "",
+      Personalidad3: "",
+      Personalidad4: "",
+      vMotivacion: "",
+      Objetivos: [],
+      Frustraciones: [],
+      Motivaciones: [],
+      Marcas: ""
+
+    }
+  },
+
+  mounted() {
+  },
+  components: {
+    btnEnviar
+  },
+  methods: {
+
+    Registro() {
+      axios
         .post("/api/guardarPersonasUxd.php", {
-  
-            nombre: this.Nombre,
-            edad: this.Edad,
-            estadoCivil: this.EstadoCivil,
-            trabajo: this.Trabajo,
-            residencia: this.Residencia,
-            cita: this.Cita,
-            citaAutor: this.CitaAutor,
-            bio: this.Bio,
-            personalidad01: this.Personalidad1,
-            personalidad02: this.Personalidad2,
-            personalidad03: this.Personalidad3,
-            personalidad04: this.Personalidad4,
-            objetivos: this.Objetivos,
-            frustraciones: this.Frustraciones,
-            motivaciones: this.Motivaciones,
-            marcas: this.Marcas
-          
-          })
-          .then((response) => {
+
+          nombre: this.Nombre,
+          edad: this.Edad,
+          estadoCivil: this.EstadoCivil,
+          trabajo: this.Trabajo,
+          residencia: this.Residencia,
+          cita: this.Cita,
+          citaAutor: this.CitaAutor,
+          bio: this.Bio,
+          personalidad01: this.Personalidad1,
+          personalidad02: this.Personalidad2,
+          personalidad03: this.Personalidad3,
+          personalidad04: this.Personalidad4,
+          objetivos: this.Objetivos,
+          frustraciones: this.Frustraciones,
+          motivaciones: this.Motivaciones,
+          marcas: this.Marcas
+
+        })
+        .then((response) => {
           console.log(response.status)
-          });
-        
-      }
-    },
-  }
-  
-  </script>
+        });
+
+    }
+  },
+}
+
+</script>
 
 
 <template>
@@ -78,48 +80,211 @@
           <!-- Div de datos personales -->
           <div class="flex">
             <div class="w-full px-3">
-              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="nombre">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Nombre">
                 Nombre:
               </label>
-              <input placeholder="Gustavo Enrique"
+              <input placeholder="ej: Gustavo Noh"
                 class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
                 type="text" name="nombre" id="nombre" v-model="Nombre">
             </div>
-            <!-- <div class="w-full px-3">
-              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="apellido">
-                Apellidos:
-              </label>
-              <input
-                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
-                id="Apellidos" type="text" placeholder="Noh Helguera">
-            </div> -->
+            <!--  Fecha de nacimiento, pendiente calcular la edad con la misma fecha  -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="fechaNac">
+                  Fecha de Nacimiento:
+                </label>
+                <input
+                  class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="date" name="fechaNac" id='fechaNac'>
+            </div>
+            <!-- Estado civil  -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="estadoCivil"> Estado
+                civil: </label>
+                <!-- select del valor de estado civil -->
+              <select
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                name="EstadoCivil" id="EstadoCivil" v-model="EstadoCivil">
+                <option value="1">Soltero</option>
+                <option value="2">Casado</option>
+                <option value="3">Divorciado</option>
+                <option value="4">Separado</option>
+                <option value="5">Union libre</option>
+                <option value="6">Viudo</option>
+              </select>
+            </div>
           </div>
-          <!-- <div class="w-full px-3">
-            <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="apellido">
-              Otro campo puedo poner aquí:
-            </label>
-            <input
-              class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
-              id="Apellidos" type="text" placeholder="Noh Helguera">
+          <!-- Div de datos personales -->
+          <div class="flex">
+            <!-- Trabajo -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="trabajo">
+                Trabajo:
+              </label>
+              <input placeholder="ej: Programador web"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="Trabajo" id="Trabajo" v-model="Trabajo">
+            </div>
+            <!-- Residencia  -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="residencia">
+                Residencia:
+              </label>
+              <input placeholder="ej: Calle 9a x 16 y 18, Telchac Pueblo"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="Residencia" id="Residencia" v-model="Residencia">
+            </div>
+          </div>
+          <!-- Div para la cita y autor -->
+          <div class="block">
+            <!-- Cita -->
+            <div class="w-full">
+              <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="residencia">
+                Cita:
+              </label>
+              <input placeholder="ej: Si la oportunidad no toca a la puerta, construye una"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="cita" id="cita" v-model="Cita">
+            </div>
+            </div>
+            <!-- Autor -->
+            <div class="flex justify-end mx-3">
+              <div>
+                <label class="block uppercase text-gray-700 text-xs font-bold py-2 mx-5" for="residencia">
+                Autor:
+                </label>
+              </div>
+              <div>
+                <input placeholder="ej: Milton Berle"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="autor" id="autor" v-model="CitaAutor">
+              </div>
+            </div>
+          </div>
+          <div class="flex w-full">
+            <!-- Div para biografia -->
+            <div class="w-full px-3 w-full">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Biografia">
+                BIOGRAFÍA:
+              </label>
+              <textarea placeholder="ej: He is the founder of design studio and graphic design blog. He specialized in logo design, branding, web design and offer design services" 
+              rows="6" cols="25" class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide" 
+              type="text" name="Bio" id="Bio" v-model="Bio"></textarea>
+            </div>
+            <!-- Div para las personalidades -->
+            <div class="w-full px-3">
+              
+              <div class="block w-full">
+                <!-- Div personalidad 1 -->
+                <div class="flex justify-end mx-3">
+                  <div>
+                    <label class="block uppercase text-gray-700 text-xs font-bold py-2 mx-5" for="Personalidad">
+                    Personalidad 1:
+                    </label>
+                  </div>
+                  <div>
+                    <input 
+                    class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                    min="0" max="100" type="range" name="personalidad" id="personalidad" v-model="Personalidad1">
+                  </div>
+                </div>
+                <!-- Div personalidad 2 -->
+                <div class="flex justify-end mx-3">
+                  <div>
+                    <label class="block uppercase text-gray-700 text-xs font-bold py-2 mx-5" for="Personalidad">
+                    Personalidad 2:
+                    </label>
+                  </div>
+                  <div>
+                    <input 
+                    class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                    min="0" max="100" type="range" name="personalidad" id="personalidad" v-model="Personalidad2">
+                  </div>
+                </div>
+                <!-- Div personalidad 3 -->
+                <div class="flex justify-end mx-3">
+                  <div>
+                    <label class="block uppercase text-gray-700 text-xs font-bold py-2 mx-5" for="Personalidad">
+                    Personalidad 3:
+                    </label>
+                  </div>
+                  <div>
+                    <input 
+                    class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                    min="0" max="100" type="range" name="personalidad" id="personalidad" v-model="Personalidad3">
+                  </div>
+                </div>
+                <!-- Div personalidad 4 -->
+                <div class="flex justify-end mx-3">
+                  <div>
+                    <label class="block uppercase text-gray-700 text-xs font-bold py-2 mx-5" for="Personalidad">
+                    Personalidad 4:
+                    </label>
+                  </div>
+                  <div>
+                    <input 
+                    class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                    min="0" max="100" type="range" name="personalidad" id="personalidad" v-model="Personalidad4">
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+          <!-- Div de datos personales -->
+          <div class="flex">
+            <!-- Marca -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Marca">
+                Marca:
+              </label>
+              <input placeholder="ej: SONY"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="marca" id="marca" v-model="Marcas">
+                <button class="px-3 py-1 rounded-lg hover:bg-primario1 bg-primario2 mb-5 text-white">Agregar nuevo</button>
+            </div>
+            <!-- Objetivos -->
+            <div class="w-full px-3">
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Objetivos">
+                Objetivos:
+              </label>
+              <input placeholder="ej: Strive for a design that's user friendly and beautiful"
+                class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                type="text" name="objetivos" id="objetivos" v-model="Objetivos">
+                <button class="px-3 py-1 rounded-lg hover:bg-primario1 bg-primario2 mb-5 text-white">Agregar nuevo</button>
+            </div>
           </div>
           <div class="flex">
+            <!-- Frustraciones  -->
             <div class="w-full px-3">
-              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="apellido">
-                Otro campo puedo poner aquí:
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Frustraciones">
+                Frustraciones:
               </label>
-              <input
+              <input placeholder="ej: expectations are not clear"
                 class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
-                id="Apellidos" type="text" placeholder="Noh Helguera">
+                type="text" name="frustraciones" id="frustraciones" v-model="Frustraciones">
+                <button class="px-3 py-1 rounded-lg hover:bg-primario1 bg-primario2 mb-5 text-white">Agregar nuevo</button>
             </div>
+            <!-- Motivaciones -->
             <div class="w-full px-3">
-              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="apellido">
-                Otro campo puedo poner aquí:
+              <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="Motivaciones">
+                Motivaciones:
               </label>
-              <input
+              <div class="flex space-x-4">
+                <input placeholder="ej: Incentive"
                 class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
-                id="Apellidos" type="text" placeholder="Noh Helguera">
+                type="text" name="motivaciones" id="motivaciones" v-model="Motivaciones">
+                <input 
+                    class="block w-full bg-white text-gray-700 border border-gray-200 rounded py-2 px-4 mx-auto mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:tracking-wide"
+                    min="0" max="100" type="range" name="personalidad" id="personalidad" v-model="Motivacion">
+              </div>
+                
+                <div class="block">
+                  <button class="px-3 py-1 rounded-lg hover:bg-primario1 bg-primario2 mb-5 text-white">Agregar nuevo</button>
+                </div>
+                
             </div>
-          </div> -->
+          </div>
 
           <btnEnviar @click="Registro"></btnEnviar>
         </div>
